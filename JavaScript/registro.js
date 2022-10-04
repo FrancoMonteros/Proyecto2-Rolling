d36f44110e28f3b6d62f1b9a82213fe6f123e633
josetrivino89 <josetrivino89@gmail.com>
2022-10-04 11:30:49 -0300

    se trabajo sobre creacion de usuario y se agrego sweet alert

--- JavaScript/registro.js
+++ JavaScript/registro.js
@@ -1,17 +1,18 @@
 //*creamos una clase
 class User{
-    constructor(){
+    constructor(nombre,email,password){
         this.nombre = nombre;
         this.email = email;
         this.password = password;
     }
 }
 //*creamos las variables de los inputs del formularios de registros*//
-let nombre = document.getElementById(`inputNombre`);
-let email = document.getElementById(`inputEmailRegistro`);
-let password = document.getElementById(`inputPasswordRegistro`);
-let formulario_Registro = document.getElementById(`RegistrarseForm`);
-console.log(formulario_Registro);
+let inputNombre = document.getElementById(`inputNombre`);
+let inputEmail = document.getElementById(`inputEmailRegistro`);
+let inputPassword = document.getElementById(`inputPasswordRegistro`);
+let inputPassword2 = document.getElementById(`inputPasswordRegistro2`);
+let form = document.getElementById(`RegistrarseForm`);
+console.log(form);
 //* creamos una variable para guardar en localStogare, o mostrar array vacio*//
 let datosUsuraios = JSON.parse(localStorage.getItem(`datosUsuraios`))||[];
 
@@ -20,27 +21,31 @@ let datosUsuraios = JSON.parse(localStorage.getItem(`datosUsuraios`))||[];
 const handleSubmit=(e)=>{
     //*evita actualizar pagina
     e.preventDefault();
- 
-
     //*creamos funciones para validar los campos del registro*//
 
 
     //* creamos un nuevo usuario:
-    const nuevoUsuario = new User(nombre.value,email.value,password.value);
+    const nuevoUsuario = new User(inputNombre.value,inputEmail.value,inputPassword.value);
     //*hago un push con los datos de los usuarios:
     console.log(nuevoUsuario);
     //*console log para ver lo que tenemos 
     datosUsuraios.push(nuevoUsuario)
     //*consulto que hay en el array
    console.log(datosUsuraios);
+   //*creamos una alerta para que el usuario separ que la creacion fue exitosa
+   Swal.fire(
+       "Usuario creado",
+       "Su usuario fue correctamente cargado",
+       "success"
+     );  
     //*mando los datos a local storage usando metoso stringify
-    localStorage.setItem(`datosUsuarios`,JSON.stringify(datosUsuraios))
+    localStorage.setItem(`datosUsuarios`,JSON.stringify(datosUsuraios))  
     //*utilizo window.location.reload para actualizar la pagina
     window.location.reload()
     //*reseteo formulario para que puedan completar siguiente producto
-    RegistrarseForm.reset()
-    /*creamos un evento para que el formulario escuche*/
+    form.reset()
+   
 }
 /*creamos un evento para que el formulario escuche*/
-RegistrarseForm.addEventListener(`submit`,handleSubmit);
+form.addEventListener(`submit`,handleSubmit);
 
